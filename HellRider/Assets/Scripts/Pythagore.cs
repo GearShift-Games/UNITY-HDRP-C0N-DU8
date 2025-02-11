1b8365bd787b5c6f5b01a4c2dd1471a86a79c8b3
sideking <jeremy-roy-cote@hotmail.com>
2025-02-10 19:31:24 -0500

    alex look at this

--- HellRider/Assets/Scripts/Pythagore.cs
+++ HellRider/Assets/Scripts/Pythagore.cs
@@ -17,11 +17,11 @@ public class Pythagore : MonoBehaviour
     {
         Vector3 currentEulerAngles = transform.rotation.eulerAngles;
 
-        float angleInRadians = currentEulerAngles.z * Mathf.Deg2Rad;
+        float angleInRadians = (currentEulerAngles.z -90 ) * Mathf.Deg2Rad;
 
         Debug.Log(Mathf.Cos(angleInRadians) * 0.5f);
         
 
-        mass.transform.localPosition = new Vector3(Mathf.Cos(angleInRadians) * 0.5f, 0, 0);
+        mass.transform.localPosition = new Vector3(Mathf.Cos(angleInRadians) * -0.5f, -0.5f, 0);
     }
 }
