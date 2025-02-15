d3ad0d66506816913dff3081e566e7f14aa82760
IanQc <ian.corbin.qc@gmail.com>
2025-02-15 16:14:48 -0500

    Son-Win-Lose

--- HellRider/Assets/WinOrLose.cs
+++ HellRider/Assets/WinOrLose.cs
@@ -9,12 +9,15 @@ public class WinOrLose : MonoBehaviour
     public GameObject MainPlayer;
     public GameObject[] Participant;
     private int LeftAlive;
-
+    public AudioSource audioSource;
+    public AudioClip LoseSound;  
+    public AudioClip WinSound;  
     void Update()
     {
         if (!MainPlayer.activeInHierarchy) //when you lose
         {
             //SceneManager.LoadScene("Circuit01_Maquette");
+            PlaySound(LoseSound);
         }
 
         LeftAlive = 0;
@@ -29,6 +32,15 @@ public class WinOrLose : MonoBehaviour
         if (LeftAlive == 1) //you win here 
         {
             SceneManager.LoadScene("Circuit01_Maquette");
+            PlaySound(WinSound);
+        }
+    }
+
+    void PlaySound(AudioClip clip)
+    {
+        if (audioSource != null && clip != null)
+        {
+            audioSource.PlayOneShot(clip);
         }
     }
 }
