0949fcd31c3a52f432ee786fffff903b6002709e
IanQc <ian.corbin.qc@gmail.com>
2025-02-26 09:47:34 -0500

    test-velo-anim

--- HellRider/Assets/Scripts/AnimatorPerso.cs
+++ HellRider/Assets/Scripts/AnimatorPerso.cs
@@ -16,11 +16,7 @@ public class AnimatorPerso : MonoBehaviour
     void Update()
     {
         // Utilise les données OSC pour ajuster la vitesse de l'animation de pédalage
-        float pedalAnimSpeed = navigationJoueur.RealSpeed * 10f; // Ajustez ce multiplicateur selon vos besoins
-        animator.SetFloat("PedalSpeed", pedalAnimSpeed);
-
-        // Si vous souhaitez aussi conserver l'ancienne méthode basée sur speedUI, vous pouvez décommenter ce qui suit :
-        // float speed = navigationJoueur.speedUI / 10f;
-        // animator.SetFloat("Speed", speed);
+        float PedalSpeed = navigationJoueur.RealSpeed * 10f; // Ajustez ce multiplicateur selon vos besoins
+        animator.SetFloat("PedalSpeed", PedalSpeed);
     }
 }
