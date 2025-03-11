d28609bf7e91fded6896d2fae52fc47c7a4c19e9
AlexandreGervaisCode <alexandregervaisme@gmail.com>
2025-03-11 10:29:12 -0400

    tutos

--- HellRider/Assets/Scripts/Tutorial-Scripts/TutoStep2.cs
+++ HellRider/Assets/Scripts/Tutorial-Scripts/TutoStep2.cs
@@ -8,21 +8,29 @@ public class TutoStep2 : MonoBehaviour
     // Animator for scene changes
     public Animator transition;
 
+    // To account both directions
+    public int directionCount = 0;
+
     private void OnTriggerEnter(Collider other)
     {
         //Debug.Log(healthPoints);
         if (other.tag == "TutoProgress")
         {
-            StartCoroutine(TutoStep1Over());
+            directionCount++;
+            other.gameObject.SetActive(false);
+            if (directionCount >= 2)
+            {
+                StartCoroutine(TutoStep2Over());
+            }
         }
     }
 
-    private IEnumerator TutoStep1Over()
+    private IEnumerator TutoStep2Over()
     {
         // Death sequence here
         transition.Play("uiFadeOUT");
         yield return new WaitForSeconds(2);
-        SceneManager.LoadScene("00b_tutorial_turn");
+        SceneManager.LoadScene("00c_tutorial_life");
         yield break;
     }
 }
