6ae71226f15fd7978a84b138af3db4d6b6fc9d0b
IanQc <ian.corbin.qc@gmail.com>
2025-03-12 11:05:19 -0400

    change for test

--- HellRider/Assets/Scripts/Pathfinding/JoueurNav2.cs
+++ HellRider/Assets/Scripts/Pathfinding/JoueurNav2.cs
@@ -112,7 +112,7 @@ public class JoueurNav2 : MonoBehaviour, IPlayerScore
     void Update()
     {
         // Récupération des données OSC et du clavier
-        RealSpeed = 0.09f; // Osc.GetComponent<OscBicycle>().Speed
+        RealSpeed = Osc.GetComponent<OscBicycle>().Speed; // 0.09f
         XValue = Osc.GetComponent<OscBicycle>().X;
         speedUI = Mathf.FloorToInt(currentSpeed);
         horizontalInput = XValue + Input.GetAxis("Horizontal");
